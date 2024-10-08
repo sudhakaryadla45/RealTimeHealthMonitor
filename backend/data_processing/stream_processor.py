3e6069d654b22bed25014c0e38d5c3505d7b3536
sudhakaryadla45 <sudhakaryadla453@gmail.com>
2024-10-09 03:16:42 +0530

    stream_processor.py

--- backend/data_processing/stream_processor.py
+++ backend/data_processing/stream_processor.py
@@ -1,16 +1,1 @@
-import time
-import random
-
-def simulate_data_stream():
-    while True:
-        data = {
-            'type': 'Heart Rate',
-            'value': random.randint(60, 100),
-            'date': time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime())
-        }
-        print(data)
-        time.sleep(1)
-
-if __name__ == '__main__':
-    simulate_data_stream()
 
